eb07f7d98ec3decc2485906ed135af2ec87f00a1
Max Hatfull <max.hatfull@freeagent.com>
2024-04-20 20:17:07 +0100

    Refactor triangle renderer

--- src/engine/triangle_renderer.rb
+++ src/engine/triangle_renderer.rb
@@ -10,46 +10,72 @@ module Engine
     end
 
     def start
+      setup_vertex_attribute_buffer
+      setup_vertex_buffer
+    end
+
+    def update(delta_time)
+      set_shader_per_frame_data
+      draw_array_data
+    end
+
+    private
+
+    def shader
+      @shader ||= Shader.new('./shaders/colour_vertex.glsl', './shaders/colour_frag.glsl')
+    end
+
+    def draw_array_data
+      shader.use
+      GL.BindVertexArray(@vao)
+      GL.DrawArrays(GL::TRIANGLES, 0, 3)
+      GL.BindVertexArray(0)
+    end
+
+    def set_shader_per_frame_data
+      shader.set_vec3("colour", colour)
+      set_shader_camera_matrix
+      set_shader_model_matrix
+    end
+
+    def set_shader_model_matrix
+      shader.set_mat4("model", game_object.model_matrix)
+    end
+
+    def set_shader_camera_matrix
+      shader.set_mat4("camera", [
+        2.0 / Engine.screen_width, 0, 0, 0,
+        0, 2.0 / Engine.screen_height, 0, 0,
+        0, 0, 1, 0,
+        -1, -1, 0, 1
+      ])
+    end
+
+    def setup_vertex_attribute_buffer
       va0_buf = ' ' * 4
       GL.GenVertexArrays(1, va0_buf)
       @vao = va0_buf.unpack('L')[0]
+      GL.BindVertexArray(@vao)
+    end
 
+    def setup_vertex_buffer
       vbo_buf = ' ' * 4
       GL.GenBuffers(1, vbo_buf)
-      @vbo = vbo_buf.unpack('L')[0]
-
-      @shader = Shader.new('./shaders/colour_vertex.glsl', './shaders/colour_frag.glsl')
-
+      vbo = vbo_buf.unpack('L')[0]
       points = [
         v1[:x], v1[:y], 0,
         v2[:x], v2[:y], 0,
         v3[:x], v3[:y], 0
       ]
 
-      GL.BindVertexArray(@vao)
-      GL.BindBuffer(GL::ARRAY_BUFFER, @vbo)
+      GL.BindBuffer(GL::ARRAY_BUFFER, vbo)
       GL.BufferData(
         GL::ARRAY_BUFFER, 3 * 3 * Fiddle::SIZEOF_FLOAT,
         points.pack('F*'), GL::STATIC_DRAW
-      ) # 3 vertices, 3 coordinates each
+      )
 
       GL.VertexAttribPointer(0, 3, GL::FLOAT, GL::FALSE, 3 * Fiddle::SIZEOF_FLOAT, 0)
       GL.EnableVertexAttribArray(0)
-    end
-
-    def update(delta_time)
-      GL.BindVertexArray(@vao)
-      @shader.use
-      @shader.set_vec3("colour", colour)
-      @shader.set_mat4("camera", [
-        2.0 / Engine.screen_width, 0, 0, 0,
-        0, 2.0 / Engine.screen_height, 0, 0,
-        0, 0, 1, 0,
-        -1, -1, 0, 1
-      ])
-      @shader.set_mat4("model", game_object.model_matrix)
-      GL.DrawArrays(GL::TRIANGLES, 0, 3)
-      GL.BindVertexArray(0)
     end
   end
 end
