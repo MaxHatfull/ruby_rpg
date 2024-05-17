5c6acc5b0b8f2f0387b4c0caecea6f11529f0938
Max Hatfull <max.hatfull@freeagent.com>
2024-05-17 18:38:02 +0100

    perspective camera visual regression test

--- spec/samples/cubes/game_objects/teapot_spec.rb
+++ spec/samples/cubes/game_objects/teapot_spec.rb
@@ -9,7 +9,7 @@ describe Cubes::Teapot do
     stub_const("ASSETS_DIR", File.expand_path(File.join(__dir__, "..", "..", "..", "..", "samples", "cubes", "assets")))
   end
 
-  describe "When placed in the scene" do
+  describe "with an orthographic camera" do
     it "renders the teapot" do
       within_game_context(load_path: "./samples/cubes") do
         at(0) do
@@ -30,7 +30,34 @@ describe Cubes::Teapot do
             ]
           )
         end
-        at(1) { check_screenshot(__dir__ + "/teapot.png") }
+        at(1) { check_screenshot(__dir__ + "/teapot_orthographic.png") }
+        at(20) { Engine.stop_game }
+      end
+    end
+  end
+
+  describe "with a perspective camera" do
+    it "renders the teapot" do
+      within_game_context(load_path: "./samples/cubes") do
+        at(0) do
+          Cubes::Teapot.new(Vector[100, 100], Vector[43, 68, 38], 500)
+          Engine::GameObject.new(
+            "light", pos: Vector[Engine.screen_width / 2, 500, 100], components: [
+            Engine::Components::PointLight.new(
+              range: 300,
+              ambient: Vector[0.5, 0.5, 0.5],
+              diffuse: Vector[1, 1, 1],
+              specular: Vector[1, 1, 1]
+            )
+          ])
+          Engine::GameObject.new(
+            "Camera", pos: Vector[Engine.screen_width / 2, Engine.screen_height / 2, 1000],
+            components: [
+              Engine::Components::PerspectiveCamera.new(fov: 45.0, aspect: Engine.screen_width / Engine.screen_height, near: 0.1, far: 2000)
+            ]
+          )
+        end
+        at(1) { check_screenshot(__dir__ + "/teapot_perspective.png") }
         at(20) { Engine.stop_game }
       end
     end
