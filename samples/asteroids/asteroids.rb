0baa56385cfb4985ca1d1be3f7f8e7c7a0c302e2
GetOutOfMyBakery <12701930+GetOutOfMyBakery@users.noreply.github.com>
2024-06-06 00:34:39 +0100

    WIP: Adding text to screen
    # What
    Adding support for displaying text on screen as a UI/for debugging.
    Also setting the window size in global vars so positioning of the UI can be relative and scaled appropriately

--- samples/asteroids/asteroids.rb
+++ samples/asteroids/asteroids.rb
@@ -1,10 +1,25 @@
 require_relative "../../src/engine/engine"
+require "pry"
 
+ROOT = File.expand_path(File.join(__dir__))
 ASSETS_DIR = File.expand_path(File.join(__dir__, "assets"))
+WINDOW_WIDTH = 1440
+WINDOW_HEIGHT = 900
 
-Engine.start(width: 1920, height: 1080, base_dir: File.dirname(__FILE__)) do
+Engine.start(width: WINDOW_WIDTH, height: WINDOW_HEIGHT, base_dir: File.dirname(__FILE__)) do
   include Asteroids
-  Ship.create(Vector[1920.0 / 2, 1080.0 / 2], 20)
+  Ship.create(Vector[WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0], 20)
+
+  # Create a camera for rendering text to the Onscreen UI
+  Engine::GameObject.new(
+    "Onscreen UI Camera",
+    pos: Vector[WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 1],
+    rotation: Vector[0, 0, 0],
+    components: [
+      Engine::Components::OrthographicCamera.new(width: WINDOW_WIDTH, height: WINDOW_HEIGHT, far: 2)
+    ]
+  )
+  OnscreenUI.create
 
   10.times do
     Asteroid.create(
