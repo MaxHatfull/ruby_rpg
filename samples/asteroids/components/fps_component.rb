37d32168f6836fcdbe59b94f138a531e19e2f454
GetOutOfMyBakery <12701930+GetOutOfMyBakery@users.noreply.github.com>
2024-07-11 01:56:22 +0100

    Display game object counter and names in FPS output

--- samples/asteroids/components/fps_component.rb
+++ samples/asteroids/components/fps_component.rb
@@ -6,8 +6,23 @@ module Asteroids
       @fps_counter = game_object.ui_renderers.first
     end
 
+    def onscreen_message
+      [
+        "FPS: #{Engine.fps.round(4)}",
+        "Total Game Objects: #{Engine::GameObject.objects.count}",
+        "",
+        "#{game_objects_tallied}"
+      ].join("\n")
+    end
+
     def update(delta_time)
-      @fps_counter.update_string("FPS: #{Engine.fps.round(4)}")
+      @fps_counter.update_string(onscreen_message)
+    end
+
+    def game_objects_tallied
+      Engine::GameObject.objects.map(&:name).tally.map do |game_obj, count|
+        "#{game_obj} (#{count})"
+      end.join("\n")
     end
   end
 end
