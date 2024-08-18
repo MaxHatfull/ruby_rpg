93aeeed911bbd8801134b297ef17c33c6014aef6
MaxHatfull <max.hatfull@freeagent.com>
2024-08-18 13:07:08 +0100

    Add skybox

--- samples/shrink_racer/shrink_racer.rb
+++ samples/shrink_racer/shrink_racer.rb
@@ -19,12 +19,5 @@ Engine.start(width: 1920, height: 1080, base_dir: File.dirname(__FILE__)) do
   RoadTrack.create
 
   car = Car.create_suv(Vector[1.5, 0.75, 0], Vector[0, 180, 0])
-  Engine::GameObject.new(
-    "Camera",
-    pos: Vector[0, 0, 0],
-    rotation: Vector[20, 180, 0],
-    components: [
-      CarFollower.new(car),
-      Engine::Components::PerspectiveCamera.new(fov: 45.0, aspect: 1920.0 / 1080.0, near: 0.1, far: 1000.0)
-    ])
+  CameraObject.create(car)
 end
