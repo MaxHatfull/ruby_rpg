4816ffe0f0b0eb8074d296e2624fd3830f0b0f93
GetOutOfMyBakery <12701930+GetOutOfMyBakery@users.noreply.github.com>
2024-08-25 21:53:36 +0100

    Some futher tweaks to 'Engine::Cursor'
    # What
    Decoupling the setting and getting of the cursor input mode.
    Still providing human_friendly methods (e.g. `Engine::Cursor.hide`, `Cursor.show`, etc.) but handling the mapping of :symbol -> GLFW::<CONSTANT>.

--- src/engine/cursor.rb
+++ src/engine/cursor.rb
@@ -4,28 +4,39 @@ module Engine
   class Cursor
     class << self
 
-      def disable
-        GLFW.SetInputMode(Window.window, GLFW::CURSOR, GLFW::CURSOR_DISABLED)
-      end
-
-      def hide
-        GLFW.SetInputMode(Window.window, GLFW::CURSOR, GLFW::CURSOR_HIDDEN)
+      def supported_modes
+        {
+          :enable   => GLFW::CURSOR_NORMAL,
+          :disable  => GLFW::CURSOR_DISABLED,
+          :hide     => GLFW::CURSOR_HIDDEN
+        }
       end
 
       def enable
-        GLFW.SetInputMode(Window.window, GLFW::CURSOR, GLFW::CURSOR_NORMAL)
+        set_input_mode(:enable)
       end
 
+      def disable
+        set_input_mode(:disable)
+      end
+
+      def hide
+        set_input_mode(:hide)
+      end
+
+      # Expects a symbol present in supported_modes, e.g. ':hide'
+      def set_input_mode(mode)
+        GLFW.SetInputMode(Window.window, GLFW::CURSOR, supported_modes[mode])
+      end
+
+      # Returns a symbol from the supported_modes, e.g. ':hide'
       def get_input_mode
-        GLFW.GetInputMode(Window.window, GLFW::CURSOR)
+        supported_modes.key(GLFW.GetInputMode(Window.window, GLFW::CURSOR))
       end
 
+      # Expects a symbol present in supported_modes, e.g. ':hide'
       def restore_input_mode(original_mode)
-        case original_mode
-        when GLFW::CURSOR_NORMAL then enable
-        when GLFW::CURSOR_HIDDEN then hide
-        when GLFW::CURSOR_DISABLED then disable
-        end
+        set_input_mode(original_mode)
       end
     end
   end
