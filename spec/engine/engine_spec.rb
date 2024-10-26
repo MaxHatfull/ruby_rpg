9746447994d68b48f6e6769a99cd6b3a3faf90f8
MaxHatfull <max.hatfull@freeagent.com>
2024-10-26 15:03:10 +0100

    Fixup tests after removing width and height settings for engine start

--- spec/engine/engine_spec.rb
+++ spec/engine/engine_spec.rb
@@ -4,11 +4,11 @@ describe Engine do
   describe ".start" do
     it "starts the game engine" do
       expect(Engine).to receive(:load).with("base_dir")
-      expect(Engine).to receive(:open_window).with(600, 800)
+      expect(Engine).to receive(:open_window)
       expect(Engine).to receive(:main_game_loop)
       expect(Engine).to receive(:terminate)
 
-      Engine.start(width: 600, height: 800, base_dir: "base_dir")
+      Engine.start(base_dir: "base_dir")
     end
   end
 end
